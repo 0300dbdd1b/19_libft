d451d762e97faf20ab9cc40b22346f816a315e41
Marcaday <noah.gr.addino@gmail.com>
2020-04-02 11:37:25 +0200

    Automatic Commit Message : Update

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,28 +6,34 @@
 /*   By: naddino <naddino@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/14 16:06:30 by naddino           #+#    #+#             */
-/*   Updated: 2020/02/06 21:54:28 by naddino          ###   ########.fr       */
+/*   Updated: 2020/03/28 19:05:11 by naddino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*output;
+    char    *output;
+    size_t    r;
 	size_t	s_len;
 
-	if (!s)
-		return (0);
 	s_len = ft_strlen(s);
-	if (s_len < start)
-		s_len = 0;
-	else
-		s_len = s_len - start;
-	if (s_len < len)
-		len = s_len;
-	if (!(output = malloc(sizeof(*s) * (len + 1)))
-			|| !ft_strlcpy(output, s + start, len + 1))
-		return (0);
-	return (output);
+    r = 0;
+    if (!s)
+        return (0);
+    if (start > s_len)
+        len = 0;
+    if (len + start > s_len && start < s_len)
+        len = (s_len - start);
+    if (!(output = malloc(sizeof(char) * (len + 1))))
+        return (0);
+    while (r < len)
+    {
+        output[r] = s[start];
+        r++;
+        start++;
+    }
+    output[r] = '\0';
+    return (output);
 }
